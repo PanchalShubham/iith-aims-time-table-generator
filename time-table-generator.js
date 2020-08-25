97b7bfec3c7850ff2ab56609a101d226e4742919
PanchalShubham <49385589+PanchalShubham@users.noreply.github.com>
2020-08-25 13:16:00 +0530

    Added semi-colons at the end

--- time-table-generator.js
+++ time-table-generator.js
@@ -3,72 +3,72 @@
  * Author: Shubham Panchal (cs18btech11052@iith.ac.in)	*
  ********************************************************/
 
-'use-strict'
-var oneTwoSegmentTable = createSegmentTable()
-var threeFourSegmentTable = createSegmentTable()
-var fiveSixSegmentTable = createSegmentTable()
-var errorMessages = []
+'use-strict';
+var oneTwoSegmentTable = createSegmentTable();
+var threeFourSegmentTable = createSegmentTable();
+var fiveSixSegmentTable = createSegmentTable();
+var errorMessages = [];
 
 function main(){
-	var programs = $('.card-body')
+	var programs = $('.card-body');
 	for (var i = 1; i < programs.length; ++i){
-		var program = programs[i]
-		var regId = program.children[0].getAttribute("data-deg-dtl")
-		process(`.stdtDegReg_${regId}_1`)
-		process(`.stdtDegReg_${regId}_2`)
-		process(`.stdtDegReg_${regId}_3`)
+		var program = programs[i];
+		var regId = program.children[0].getAttribute("data-deg-dtl");
+		process(`.stdtDegReg_${regId}_1`);
+		process(`.stdtDegReg_${regId}_2`);
+		process(`.stdtDegReg_${regId}_3`);
 	}
-	parseTable()
+	parseTable();
 }
-main()
+main();
 	
 
 function process(className){
-	var courses = $(className)
+	var courses = $(className);
 	for (var i = 0; i < courses.length; ++i){
-		const course = courses[i]
-		if (course === undefined)	continue
-		if (course.children.length < 9)	continue
-		const children = course.children
-		const courseID = children[7].children[0].children[0].title
-		const courseTitle = children[8].children[0].title
-		const timeTableButton = children[7].children[3]
-		timeTableButton.click()
-		const timeTableID = course.children[7].children[3].id.replace("timeTab", "timeTabTr")
-		const timeTable = $(`.${timeTableID}`)
+		const course = courses[i];
+		if (course === undefined)	continue;
+		if (course.children.length < 9)	continue;
+		const children = course.children;
+		const courseID = children[7].children[0].children[0].title;
+		const courseTitle = children[8].children[0].title;
+		const timeTableButton = children[7].children[3];
+		timeTableButton.click();
+		const timeTableID = course.children[7].children[3].id.replace("timeTab", "timeTabTr");
+		const timeTable = $(`.${timeTableID}`);
 		for (var j = 0; j < timeTable.length; ++j){
-			const lecture = timeTable[j]
+			const lecture = timeTable[j];
 			if (lecture.children.length < 3){
-				errorMessages.push(`No time table available for ${courseID}(${courseTitle})`)
-				continue
+				errorMessages.push(`No time table available for ${courseID}(${courseTitle})`);
+				continue;
 			}
-			const segment = lecture.children[1].textContent
-			const timing = lecture.children[2].children[1].textContent
-			addToTable(courseID, courseTitle, segment, timing)
+			const segment = lecture.children[1].textContent;
+			const timing = lecture.children[2].children[1].textContent;
+			addToTable(courseID, courseTitle, segment, timing);
 		}
-		timeTableButton.click()
+		timeTableButton.click();
 	}
 }
 
 
 
 function addToTable(courseId, courseTitle, segment, timing){
-	const cellText = `${courseId}(${courseTitle})`
-	const pieces = timing.split('-')
-	const day = pieces[0]
-	const start = pieces[1]
-	const end = pieces[2]
+	const cellText = `${courseId}(${courseTitle})`;
+	const pieces = timing.split('-');
+	const day = pieces[0];
+	const start = pieces[1];
+	const end = pieces[2];
 	const startTime = ['09:00','10:00','11:00','12:00',
-						'14:30','16:00','18:00','19:30']	
+						'14:30','16:00','18:00','19:30'];	
 	const endTime = ['09:55','10:55','11:55','12:55',
-						'15:55','17:25','19:25','21:00']
-	const classesTime = []
+						'15:55','17:25','19:25','21:00'];
+	const classesTime = [];
 	for (var i = 0; i < startTime.length; ++i)
 		if (startTime[i] === start)
-			classesTime.push(startTime[i] + "-" + endTime[i])
+			classesTime.push(startTime[i] + "-" + endTime[i]);
 	for (var i = 0; i < endTime.length; ++i)
 		if (endTime[i] === end)
-			classesTime.push(startTime[i] + "-" + endTime[i])
+			classesTime.push(startTime[i] + "-" + endTime[i]);
 
 	switch (segment){
 		case '1-2':
@@ -118,34 +118,34 @@ function addToTable(courseId, courseTitle, segment, timing){
 
 function addClass(timeTable, day, classTime, cellText){
 	for (var i = 0; i < classTime.length; ++i){
-		let time = classTime[i]
-		timeTable[day][time] = cellText
+		let time = classTime[i];
+		timeTable[day][time] = cellText;
 	}
-	return timeTable
+	return timeTable;
 }
 
 function parseTable(){
-	let div = document.createElement("div")
-	div.id = "printTimeTableDiv"
+	let div = document.createElement("div");
+	div.id = "printTimeTableDiv";
 	div.style = "font-size: 20px;" + 
 				"text-align: left;" + 
 				"background-color: white;" +
-				"padding: 10px"
-	let innerHtmlText = `Report generated on ${new Date()}<br>`
-	innerHtmlText = innerHtmlText + "If you find any discrepancy please write to <strong><em>cs18btech11052@iith.ac.in</em></strong><br>"
-	innerHtmlText = innerHtmlText + "Press Ctrl + P to download in pdf format<br><br>"
+				"padding: 10px";
+	let innerHtmlText = `Report generated on ${new Date()}<br>`;
+	innerHtmlText = innerHtmlText + "If you find any discrepancy please write to <strong><em>cs18btech11052@iith.ac.in</em></strong><br>";
+	innerHtmlText = innerHtmlText + "Press Ctrl + P to download in pdf format<br><br>";
 	if (errorMessages.length > 0){
-		innerHtmlText = innerHtmlText + "Following problems were detected:<br>"
-		innerHtmlText = innerHtmlText + `<span style='color: red'>${errorMessages.join("<br>")}</span><br><br>`
+		innerHtmlText = innerHtmlText + "Following problems were detected:<br>";
+		innerHtmlText = innerHtmlText + `<span style='color: red'>${errorMessages.join("<br>")}</span><br><br>`;
 	}
-	innerHtmlText = innerHtmlText + "<span>Time table for segment 1-2</span><br>"
-	innerHtmlText = innerHtmlText + generateTable(oneTwoSegmentTable)
-	innerHtmlText = innerHtmlText + "<span>Time table for segment 3-4</span><br>"
-	innerHtmlText = innerHtmlText + generateTable(threeFourSegmentTable)
-	innerHtmlText = innerHtmlText + "<span>Time table for segment 5-6</span><br>"
-	innerHtmlText = innerHtmlText + generateTable(fiveSixSegmentTable)
-	div.innerHTML = innerHtmlText
-	var newWindow = window.open()
+	innerHtmlText = innerHtmlText + "<span>Time table for segment 1-2</span><br>";
+	innerHtmlText = innerHtmlText + generateTable(oneTwoSegmentTable);
+	innerHtmlText = innerHtmlText + "<span>Time table for segment 3-4</span><br>";
+	innerHtmlText = innerHtmlText + generateTable(threeFourSegmentTable);
+	innerHtmlText = innerHtmlText + "<span>Time table for segment 5-6</span><br>";
+	innerHtmlText = innerHtmlText + generateTable(fiveSixSegmentTable);
+	div.innerHTML = innerHtmlText;
+	var newWindow = window.open();
 	let documentText = "<!DOCTYPE html>" + 
 						"<html>" + 
 						"<head>" + 
@@ -164,44 +164,39 @@ function parseTable(){
 							"<body>" + 
 								div.innerHTML +  
 							"</body>" + 
-						"</html>"
-	newWindow.document.write(documentText)	
+						"</html>";
+	newWindow.document.write(documentText);
 }
 
 function generateTable(timeTable){
-	const monday = timeTable['Monday']
-	const tuesday = timeTable['Tuesday']
-	const wednesday = timeTable['Wednesday']
-	const thursday = timeTable['Thursday']
-	const friday = timeTable['Friday']
 	const days = ['Monday', 'Tuesday', 'Wednesday',
-					'Thursday', 'Friday']
+					'Thursday', 'Friday'];
 	let timings = ['09:00-09:55','10:00-10:55',
 					'11:00-11:55','12:00-12:55',
 					'14:30-15:55','16:00-17:25',
-					'18:00-19:25','19:30-21:00']
+					'18:00-19:25','19:30-21:00'];
 	let htmlText = "<table style='font-size: 15px;'>" + 
 							"<tr>" + 
-								"<td style='background-color: #E2E3E5'>" + '' + "</td>"
+								"<td style='background-color: #E2E3E5'>" + '' + "</td>";
 	for (var i = 0; i < timings.length; ++i)
-		htmlText = htmlText + 	"<td style='background-color: #E2E3E5'>" + timings[i] + "</td>"
-	htmlText = htmlText + 	"</tr>"
+		htmlText = htmlText + 	"<td style='background-color: #E2E3E5'>" + timings[i] + "</td>";
+	htmlText = htmlText + 	"</tr>";
 
 	for (var i = 0; i < days.length; ++i){
-		let day = days[i]
-		schedule = timeTable[day]
-		htmlText = htmlText + "<tr>"
-		htmlText = htmlText + "<td style='background-color: #E2E3E5'>" + day + "</td>"
+		let day = days[i];
+		schedule = timeTable[day];
+		htmlText = htmlText + "<tr>";
+		htmlText = htmlText + "<td style='background-color: #E2E3E5'>" + day + "</td>";
 		for (var j = 0; j < timings.length; ++j){
-			let timing = timings[j]
-			htmlText = htmlText + "<td>" + schedule[timing] + "</td>"
+			let timing = timings[j];
+			htmlText = htmlText + "<td>" + schedule[timing] + "</td>";
 		}
-		htmlText = htmlText + "</tr>"		
+		htmlText = htmlText + "</tr>";		
 	}
 	htmlText = htmlText + 
 					"</table>" + 
-					"<br> <br>"
-	return htmlText
+					"<br> <br>";
+	return htmlText;
 }
 
 function createSegmentTable(){
